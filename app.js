ef56377f7b65a94f94099503af7118d00bbe3b69
sooyGcool <141377186+sooyGcool@users.noreply.github.com>
2023-08-06 10:31:18 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const play = document.getElementById("play");
+
+play.addEventListener("click", function(){
+    swal( {
+        title: "the game is in work",
+        buttons: false,
+        className: "swal",
+    });
+});
